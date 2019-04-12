bce8a24c926f7014b01f5053d88a926cdca782c7
lvhouyang <lvhouyang@qq.comm>
2019-04-12 10:04:57 +0800

    1

--- js/footer.js
+++ js/footer.js
@@ -1,59 +1,33 @@
 //返回顶部
-window.onload = function(){
-
+window.onload = imgFade;
+window.onscroll = imgFade;
+$(".return-top").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 500);
+})
+function imgFade() {
     var eleTop = $('.concert-show').offset().top;
     var isFirst = true;
     // window.onscroll = function(){
-        if($(document.documentElement).scrollTop()>2150){
-            $(".return-top").show();
-        }else{
-            $(".return-top").hide();
-        }
-        var cliHeight = $(window).height();
-        var scroll = $(document.documentElement).scrollTop() + cliHeight;
-        if( scroll > eleTop && isFirst==true){
-            $('.concert-show ul li').eq(1).addClass('fadeLeft')
-            $('.concert-show ul li').eq(0).addClass('fadeLeft')
-            $('.concert-show ul li').eq(4).addClass('fadeLeft')
-            $('.concert-show ul li').eq(5).addClass('fadeLeft')
-            $('.concert-show ul li').eq(2).addClass('fadeRight')
-            $('.concert-show ul li').eq(3).addClass('fadeRight')
-            $('.concert-show ul li').eq(6).addClass('fadeRight')
-            $('.concert-show ul li').eq(7).addClass('fadeRight')
-            isFirst = false;     
-        }
-
-
-    window.onscroll = function(){
-        var eleTop = $('.concert-show').offset().top;
-        var isFirst = true;
-        // window.onscroll = function(){
-            if($(document.documentElement).scrollTop()>2150){
-                $(".return-top").show();
-            }else{
-                $(".return-top").hide();
-            }
-            var cliHeight = $(window).height();
-            var scroll = $(document.documentElement).scrollTop() + cliHeight;
-            if( scroll > eleTop && isFirst==true){
-                $('.concert-show ul li').eq(1).addClass('fadeLeft')
-                $('.concert-show ul li').eq(0).addClass('fadeLeft')
-                $('.concert-show ul li').eq(4).addClass('fadeLeft')
-                $('.concert-show ul li').eq(5).addClass('fadeLeft')
-                $('.concert-show ul li').eq(2).addClass('fadeRight')
-                $('.concert-show ul li').eq(3).addClass('fadeRight')
-                $('.concert-show ul li').eq(6).addClass('fadeRight')
-                $('.concert-show ul li').eq(7).addClass('fadeRight')
-                isFirst = false;     
-            }
-        // }
-    
-    
-        $(".return-top").click(function () {
-            $("html,body").animate({scrollTop:0}, 500);
-        })
+    if ($(document.documentElement).scrollTop() > 2150) {
+        $(".return-top").show();
+    } else {
+        $(".return-top").hide();
+    }
+    var cliHeight = $(window).height();
+    var scroll = $(document.documentElement).scrollTop() + cliHeight;
+    if (scroll > eleTop && isFirst == true) {
+        $('.concert-show ul li').eq(1).addClass('fadeLeft')
+        $('.concert-show ul li').eq(0).addClass('fadeLeft')
+        $('.concert-show ul li').eq(4).addClass('fadeLeft')
+        $('.concert-show ul li').eq(5).addClass('fadeLeft')
+        $('.concert-show ul li').eq(2).addClass('fadeRight')
+        $('.concert-show ul li').eq(3).addClass('fadeRight')
+        $('.concert-show ul li').eq(6).addClass('fadeRight')
+        $('.concert-show ul li').eq(7).addClass('fadeRight')
+        isFirst = false;
     }
 }
-   
-  
+
+
+
 
